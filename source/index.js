5db9cb7442b105e80f985ac6b37c3c1dbb1b4166
Eric Elliott <eelliott@dronedeploy.com>
2016-10-04 18:34:01 -0600

    Target ES5

--- source/index.js
+++ source/index.js
@@ -2,19 +2,22 @@
  * Sugar for myString.split('.');
  */
 // dp(path: String, delimiter: String) => Array
-const dp = (input, delimiter) => {
+var dp = function dp (input, delimiter) {
   var d = delimiter || '.';
   return input.split(d);
 };
 
+dp.dp = dp;
+
 // get(path: String, i: Immutable) => value: Any
-const get = (path, i) => i.getIn(dp(path));
+dp.get = function get (path, i) {
+  return i.getIn(dp(path));
+};
 
 // set(path: String, i: Immutable, val: Any) => Immutable
-const set = (path, i, val) => i.setIn(dp(path), val);
+dp.set = function set (path, i, val) {
+  return i.setIn(dp(path), val);
+};
 
 module.exports = dp;
-module.exports.dp = dp;
 module.exports.default = dp;
-module.exports.get = get;
-module.exports.set = set;
